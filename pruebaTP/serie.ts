406d196e6f379fa73fbd09fed0f36f6de80b04b0
aneira3 <111013784+aneira3@users.noreply.github.com>
2023-10-23 02:05:25 -0500

    Delete pruebaTP directory

--- pruebaTP/serie.ts
+++
@@ -1,7 +0,0 @@
-export class serie{
-
-    constructor(public position: number, public title: string, public platform: string, public nr_seasons:number, public description:string, public direction:string, public image:string)
-    {
-
-    }
-}
